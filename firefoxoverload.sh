7ea238952bded74b4ea5135a8e5a3a04e9264beb
richardkaler <31756802+richardkaler@users.noreply.github.com>
2024-02-23 01:11:01 -0500

    Update firefoxoverload.sh

--- firefoxoverload.sh
+++ firefoxoverload.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 # Copyright 2024 Richard Kaler 
-#
+
 # This script is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published
 # by the Free Software Foundation; either version 3 of the License, or (at
 # your option) any later version.
-#
+
 # This script is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
